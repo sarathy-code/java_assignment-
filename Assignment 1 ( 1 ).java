172491b3e9a75a038ce0080848a54531f27b131d
sarathy-code <sarathymurugesan@outlook.com>
2024-11-12 09:56:02 +0530

    Update Assignment 1 ( 1 ).java

--- Assignment 1 ( 1 ).java
+++ Assignment 1 ( 1 ).java
@@ -24,9 +24,9 @@ public class ContainerWithMostWater {
     public static void main(String[] args) {
         ContainerWithMostWater solution = new ContainerWithMostWater();
         int[] height1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-        System.out.println("Max area: " + solution.maxArea(height1)); // Output: 49
+        System.out.println("Max area: " + solution.maxArea(height1)); 
         
         int[] height2 = {1, 1};
-        System.out.println("Max area: " + solution.maxArea(height2)); // Output: 1
+        System.out.println("Max area: " + solution.maxArea(height2)); 
     }
 }
